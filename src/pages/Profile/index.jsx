f6a8ee82c8145c6f3cfd2d957a18c114a29379ae
Wmissary <pro.wmissary@gmail.com>
2023-07-31 17:20:15 +0200

    chore: create UserData class, create Profile page, create API mock, start Profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+import fetchProfileData from "../../services/fetchProfileData";
+import UserData from "../../classes/UserData";
+
+import { kUserDataMock } from "../../mock/userData";
+
+export default function Profile() {
+  const [profileData, setProfileData] = useState(null);
+  useEffect(() => {
+    //const kApiUrl = new URL("https://api.github.com/users/");
+    //fetchProfileData(kApiUrl).then(data => setProfileData(data));
+    const userData = new UserData({
+      id: kUserDataMock.id,
+      firstName: kUserDataMock.userInfos.firstName,
+      lastName: kUserDataMock.userInfos.lastName,
+      age: kUserDataMock.userInfos.age,
+      todayScore: kUserDataMock.todayScore,
+      calorieCount: kUserDataMock.keyData.calorieCount,
+      proteinCount: kUserDataMock.keyData.proteinCount,
+      carbohydrateCount: kUserDataMock.keyData.carbohydrateCount,
+      lipidCount: kUserDataMock.keyData.lipidCount,
+    });
+    setProfileData(userData);
+  }, []);
+
+  return (
+    <div className="Profile">
+      <h1>{`Bonjour ${profileData.firstName}`}</h1>
+    </div>
+  );
+}
