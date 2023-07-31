d826614b45119d7c3bb377be138161f6bfb7d82e
Wmissary <pro.wmissary@gmail.com>
2023-07-31 16:51:43 +0200

    chore: clean repo & add fetchProfile service

---
+++ src/services/fetchProfileData.js
@@ -0,0 +1,9 @@
+export default async function fetchProfileData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
